6257af6a3c9c58a58931b2511b44c8095c085a31
李瑶 <1663846310@qq.com>
2019-11-27 10:41:01 +0800

    学车不

---
+++ base/base_page.py
@@ -0,0 +1,23 @@
+"""
+基类
+"""
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def find_element_func(self, location, timeout=10, poll=1):
+        """获取元素定位方法"""
+        WebDriverWait(self.driver, timeout, poll).until(lambda x: x.find_element(*location))
+
+    def click_func(self, location):
+        # 元素点击方法
+        self.find_element_func(location).click()
+
+    def input_func(self, location, text):
+        # 元素输入方法
+        element = self.find_element_func(location)
+        element.clear()
+        element.send_keys(text)
